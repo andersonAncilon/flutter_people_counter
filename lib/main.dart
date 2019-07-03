1f68a3842f46fa9ba093888be785505cf7715833
anderson.santos <DAnnath01>
2019-07-03 13:35:44 -0300

    Final commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,36 +6,84 @@ void main() {
   runApp(MaterialApp(
       //alguns parametros opcionais
       title: "Contador de pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Pessoas: 0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "+1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
-              ),
-              FlatButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          Text("Pode entrar!",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0))
-        ],
-      )));
+      home: Home()));
 }
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people = 0;
+  String _infoText = "Pode entrar";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      if(_people < 0) {
+        _infoText = "Mundo invertido";
+      }
+      else if (_people <= 10) {
+        _infoText = "Pode entrar";
+      }
+      else {
+        _infoText = "Lotado!!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset("images/restaurant.jpg", fit: BoxFit.cover, height: 1000.0,),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas: $_people",
+                style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                )
+              ],
+            ),
+            Text(_infoText,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0))
+          ],
+        )
+      ],
+    );
+  }
+}
+
